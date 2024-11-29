79e88255f0f3e83f5e76410a526cc86c8fc23c66
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-29 19:35:58 -0300

    feat: add api call to get todos from jsonplaceholder api using axios

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { api } from './services/api';
+
+export type Todo = {
+	id: number;
+	title: string;
+	completed: boolean;
+};
+
 export function App() {
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	useEffect(() => {
+		api.get('/todos').then((response) => setTodos(response.data));
+	}, []);
+
 	return (
 		<div className="min-h-screen bg-zinc-950 text-zinc-50">
 			<h1>Todos</h1>
+
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						{todo.id} - {todo.title} - <span>Completed?</span> -{' '}
+						{todo.completed ? '✅ Done' : '⛔️ Not completed yet'}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
